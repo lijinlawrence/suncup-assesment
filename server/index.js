1ce60144cbf5452000a238a9c099abb520a165f1
lijinlawrence <kittulawrence@gmail.com>
2026-05-19 16:40:43 +0530

    code updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middlewares
+// Middlewares 
 app.use(cors({
   origin: '*', // Allow all origins for testing/assessment simplicity
   methods: ['GET', 'POST'],
